a4f87d4202f6502839db15b9d92c7f6bf006172b
w_sunxiaoyu@xiwang.com <w_sunxiaoyu@xiwang.com>
2024-12-04 09:19:55 +0800

    ✨ feat: 多个useState的demo

--- reactTs/src/page/demos/demo1/common/index.tsx
+++ reactTs/src/page/demos/demo1/common/index.tsx
@@ -1,9 +1,9 @@
 import { useEffect } from "react";
 
-export const RenderDemo1 = (nn: string) => {
+export const RenderDemo1 = (nn: string, showRenderDemo1: boolean) => {
   useEffect(() => {
     console.log("useDemo");
   }, []);
 
-  return <div>我是RenderDemo1{nn}</div>;
+  return <div>{showRenderDemo1 && `我是RenderDemo1${nn}`}</div>;
 };
--- reactTs/src/page/demos/demo1/index.tsx
+++ reactTs/src/page/demos/demo1/index.tsx
@@ -4,12 +4,17 @@ import { RenderDemo1 } from "./common";
 const Demo1 = () => {
 
   const [ showRenderDemo1, setShowRenderDemo1 ] = useState(false);
+  const [ showRenderDemo2, setShowRenderDemo2 ] = useState(false);
 
   return (
     <div>
       <button onClick={() => setShowRenderDemo1(!showRenderDemo1)}>Toggle RenderDemo1</button>
       {
-        showRenderDemo1 && RenderDemo1('N')
+        RenderDemo1('N', showRenderDemo1)
+      }
+       <button onClick={() => setShowRenderDemo2(!showRenderDemo2)}>Toggle RenderDemo1</button>
+      {
+        RenderDemo1('M', showRenderDemo2)
       }
     </div>
   );
