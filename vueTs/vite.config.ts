dfbcf2b8ada66aecf3d0df10ca60412027c2b20b
w_sunxiaoyu <w_sunxiaoyu@xiwang.com>
2025-02-08 14:34:28 +0800

    ✨ feat: 更改两个项目的端口号

--- vueTs/vite.config.ts
+++ vueTs/vite.config.ts
@@ -4,6 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 7474
+  },
   // 配置别名@指向src目录
   resolve: {
     alias: {
