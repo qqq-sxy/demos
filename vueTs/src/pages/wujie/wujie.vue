4522d2014bd2121db2211742159fe12a4596633a
sunxiaoyu <3469998737@qq.com>
2025-11-27 23:25:15 +0800

    feat:  初始化跳转路由也完成了

--- vueTs/src/pages/wujie/wujie.vue
+++ vueTs/src/pages/wujie/wujie.vue
@@ -22,7 +22,7 @@ const { bus } = WujieVue;
 
 // 子应用配置（React 子应用的运行地址，后续启动子应用后会用到）
 const subAppName = ref("react-sub-app"); // 子应用唯一标识（必须唯一）
-const subAppUrl = ref("http://localhost:1074"); // 子应用端口（后续配置 React 子应用为 3001）
+const subAppUrl = ref("http://localhost:1074/#/wujieDemo1"); // 子应用端口（后续配置 React 子应用为 3001）
 
 // 主应用向子应用传递的 props（可选）
 const subAppProps = ref({
@@ -36,6 +36,7 @@ bus.$on("subAppData", (data: string) => {
   console.log('data: ', data);
 });
 /** 监听子应用的数据 */
+
 
 watch(
   () => router.currentRoute.value.meta.subAppPath,
